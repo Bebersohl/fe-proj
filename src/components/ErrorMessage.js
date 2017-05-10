f91055364ba9402601532d7117df4b4a4eb6425f
Brandon <Brandon@mindframe.com>
2017-05-10 16:38:31 -0500

    added form validation

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Message } from 'semantic-ui-react'
+import './ErrorMessage.css'
+
+const ErrorMessage = (props) => (
+  <Message
+    error
+    {...props}
+  />
+)
+
+export default ErrorMessage
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import { Segment, Button, Form, Header, Icon, Divider, Message } from 'semantic-ui-react'
+import ErrorMessage from './ErrorMessage'
+import {validate} from 'email-validator'
 
 class SignUp extends Component {
   state = {
@@ -9,19 +11,36 @@ class SignUp extends Component {
     emailError: false,
     passwordError: false,
     confirmPasswordError: false,
-    errorMessages: [{
-      header: 'hi',
-      content: 'hello',
-    }],
+    errorMessages: [],
   }
   handleSubmit = (e) => {
     e.preventDefault()
     const {email, password, confirmPassword,} = this.state
+    const errorMessages = []
+
+    if(!email){
+      errorMessages.push('Email is required')
+    } else if (!validate(email)){
+      errorMessages.push('Email is invalid')
+    }
+
+    if(!password){
+      errorMessages.push('Password is required')
+    } else if (password < 6) {
+      errorMessages.push('Password must be at least 6 characters long')
+    }
+
+    if(!confirmPassword){
+      errorMessages.push('Confirm Password is required')
+    } else if (password !== confirmPassword) {
+      errorMessages.push('Password\'s must match')
+    }
 
     this.setState({
-      emailError: !email,
-      passwordError: !password,
-      confirmPasswordError: !confirmPassword,
+      emailError: !email || !validate(email),
+      passwordError: !password || password.length < 6,
+      confirmPasswordError: !confirmPassword || confirmPassword !== password,
+      errorMessages,
     })
   }
   render() {
@@ -37,16 +56,11 @@ class SignUp extends Component {
           Sign Up
         </Header>
         <Form error={formHasError} onSubmit={e => this.handleSubmit(e)}>
-          {this.state.errorMessages.map((message, index) => (
-            <div key={index}>
-              <Message
-                error
-                header={message.header}
-                content={message.content}
-              />
-              <br/>
-            </div>
-          ))}
+          
+          <ErrorMessage
+            list={this.state.errorMessages}
+          />
+
           <Form.Input
             label='Email'
             placeholder="Email"
